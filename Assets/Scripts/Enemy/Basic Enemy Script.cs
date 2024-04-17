6a447ca5fc465ced8a71b07ab2aa681defe7e2e7
JackPears0n <529831@chrome.escg.ac.uk>
2024-04-17 17:35:47 +0100

    Asset Import 3
    Added Study: Husk model
    Added Study: Abyss Arachnid model
    
    Changed the basic enemy script to find player object instead of it being set in Unity. Should stop any problems with finding plyer when loaded from a prefab.

--- Assets/Scripts/Enemy/Basic Enemy Script.cs
+++ Assets/Scripts/Enemy/Basic Enemy Script.cs
@@ -22,6 +22,7 @@ public class BasicEnemyScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        target = GameObject.Find("Player Object");
         agent = GetComponent<NavMeshAgent>();
     }
 
